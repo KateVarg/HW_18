803bf1cb8c1ad9f67022216dbd2e7570fa3a83ba
KateVarg <katye9470@gmail.com>
2024-07-16 23:56:23 +0200

    First commit

---
+++ demowebshop_test/test_data/data.py
@@ -0,0 +1,5 @@
+ID_PRODUCT_1 = '14/1/1'
+NAME_1 = 'Black & White Diamond Heart'
+
+ID_PRODUCT_2 = '31/1/1'
+NAME_2 = '14.1-inch Laptop'
---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selene import browser
+from utils import attach
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_setting():
+    browser.config.base_url = 'https://demowebshop.tricentis.com/cart'
+    browser.config.timeout = 10.0
+    browser.config.window_height = 1800
+    browser.config.window_width = 1200
+
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+
+    browser.quit()
